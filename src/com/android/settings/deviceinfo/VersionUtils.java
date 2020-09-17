c31c56ec4fc23d0bbcb8116be271eebf432d84b1
Rajkale99 <kaleraj.rk@gmail.com>
2020-09-17 20:14:20 +0000

    ro.cannedversion defines Canned Version now
    Change-Id: Iae51714516f476687d055a9343914847d30fd339

--- src/com/android/settings/deviceinfo/VersionUtils.java
+++ src/com/android/settings/deviceinfo/VersionUtils.java
@@ -5,7 +5,7 @@ import android.os.SystemProperties;
 
 public class VersionUtils {
     public static String getCannedVersion(){
-        String buildType = SystemProperties.get("ro.canned.version","");
+        String buildType = SystemProperties.get("ro.cannedversion","");
 	return buildType.equals("") ? "" : buildType;
     }
 }
