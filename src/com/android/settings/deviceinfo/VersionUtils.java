rajkale99/packages_apps_Settings-1

Fix VersionUtil

src/com/android/settings/deviceinfo/VersionUtils.java
@@ -5,7 +5,7 @@ import android.os.SystemProperties;
 
 public class VersionUtils {
     public static String getLegionVersion(){
-        String buildType = SystemProperties.get("ro.legion.build.version","");
+        String buildType = SystemProperties.get("ro.legion.version","");
 	return buildType.equals("") ? "" : buildType;
     }
 }

CannedOS/packages_apps_Settings

Change-Id: Iabfc0b4262462584aee41fcd0bc44a280f234b3b

src/com/android/settings/deviceinfo/VersionUtils.java
@@ -5,7 +5,7 @@ import android.os.SystemProperties;
 
 public class VersionUtils {
     public static String getCannedVersion(){
-        String buildType = SystemProperties.get("ro.canned.build.version","");
+        String buildType = SystemProperties.get("ro.canned.version","");
 	return buildType.equals("") ? "" : buildType;
     }
 }